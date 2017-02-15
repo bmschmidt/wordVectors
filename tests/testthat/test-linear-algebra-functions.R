54cbd0282c41ffa8031d6283bbb4ac439a9c0630
Ben Schmidt <bmschmidt@gmail.com>
2017-02-15 15:16:02 -0500

    bug fixes

--- tests/testthat/test-linear-algebra-functions.R
+++ tests/testthat/test-linear-algebra-functions.R
@@ -17,5 +17,12 @@ test_that("Distance is between 0 and 2 (pt 1)",
 test_that("Distance is between 0 and 2 (pt 1)",
           expect_lt(
             max(cosineDist(demo_vectors,demo_vectors)),
-            2 + 1e07)
+            2 + 1e-07)
           )
+
+
+test_that("Distance is between 0 and 2 (pt 1)",
+          expect_lt(
+            max(abs(1-square_magnitudes(normalize_lengths(demo_vectors)))),
+            1e-07)
+)
